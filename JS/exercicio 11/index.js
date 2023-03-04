c8a495eadf49a040d83a1a7e0c6f32804e392349
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-04 13:00:34 +0900

    exercicio
    exercicio

---
+++ JS/exercicio 11/index.js
@@ -0,0 +1,16 @@
+function testeButton(){
+  document.body.style.backgroundColor="blue";
+}
+function testeButtonRed(){
+  document.body.style.backgroundColor="red";
+}
+function testeButtonYellow(){
+  document.body.style.backgroundColor="yellow";
+}
+function testeButtonGreen(){
+  document.body.style.backgroundColor="Green";
+}
+function testeButtonWhite(){
+  document.body.style.backgroundColor="White";
+}
+
