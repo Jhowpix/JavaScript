60402a35079443ecfa477ba9647fcbae7a9fe13d
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-09 18:47:09 +0900

    modulos
    praticando com módulos

--- JS-/exercicio 1 modulos/index.js
+++ JS-/exercicio 1 modulos/index.js
@@ -1,17 +1,33 @@
 import {soma, areaQuad } from "./funcoes.js";
 import {arr, exibirPro} from "./script.js"
+import areas from "./variosExports.js"
 
 console.log(soma())
 console.log(areaQuad())
-
 console.log('-------------------------')
 
 console.log(soma)
 console.log(areaQuad)
-
 console.log('-------------------------')
 
 console.log(arr)
 console.log('-------------------------')
 
 console.log(exibirPro())
+console.log('-------------------------')
+
+console.log(areas)
+console.log(areas.areaQuad)
+console.log(areas.areaQuad())
+console.log('-------------------------')
+
+
+console.log(areas.areaTriangulo)
+console.log(areas.areaTriangulo())
+console.log('-------------------------')
+
+
+console.log(areas.areaCirculo)
+console.log(areas.areaCirculo())
+console.log('-------------------------')
+
---
+++ JS-/exercicio 1 modulos/variosExports.js
@@ -0,0 +1,23 @@
+function areaQuad(){
+    let num = 4 * 8;
+    return num
+}
+
+function areaTriangulo(){
+    let num = 5 * 9;
+    return num
+}
+
+function areaCirculo(){
+    let num = 11 * 3.14;
+    return num
+}
+
+const areas ={
+    areaQuad,
+    areaTriangulo,
+    areaCirculo
+
+}
+
+export default areas;
